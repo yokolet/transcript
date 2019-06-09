88ed43c1ac9ab877e0ab8f6965a0d133f8ad5f91
YOKO HARADA <yokolet@gmail.com>
2019-06-08 19:42:23 -0700

    feat: adds main function and its test
    The main function to go over all process was added. Now, English word
    list can be converted to Katakana list.

---
+++ eng_to_kana/eng_to_kana.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import pickledb
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+from vowel_converter import VowelConverter
+from consonant_converter import ConsonantConverter
+from epenthetic_vowel_handler import EpentheticVowelHandler
+from morae_creator import MoraeCreator
+from morae_kana_converter import MoraeKanaConverter
+
+class EngToKana:
+    def __init__(self):
+        dbname = os.path.join(os.path.dirname(__file__), '..', 'cmu_dict', 'cmu_ipa.pickle')
+        self.db = pickledb.load(dbname, False)
+        self.vowel_fn = VowelConverter().convertVowel
+        self.consonant_fn = ConsonantConverter().convertConsonant
+        self.epenthetic_fn = EpentheticVowelHandler().addEpentheticVowel
+        self.morae_fn = MoraeCreator().createMorae
+        self.kana_fn = MoraeKanaConverter().convertMorae
+
+    def transcript(self, word):
+        try:
+            phs = self.db.get(word)
+            if not phs:
+                return []
+            phs1 = [self.vowel_fn(word, ph) for ph in phs]
+            phs2 = [self.consonant_fn(word, ph) for ph in phs1]
+            phs3 = [self.epenthetic_fn(ph) for ph in phs2]
+            moraes = [self.morae_fn(ph) for ph in phs3]
+            return [self.kana_fn(m) for m in moraes]
+        except KeyError:
+            return []
+
+    def fromFile(self, filename):
+        print('[process words in {}]'.format(filename))
+
+    def fromWordList(self, words):
+        return [self.transcript(w) for w in words]
+
+def process_args():
+    parser = argparse.ArgumentParser(
+        prog='eng_to_kana.py',
+        description='Transcripts English word(s) to Katakana'
+    )
+    parser.add_argument('word',
+                        nargs='*',
+                        help='space separated word list to be transcripted')
+    parser.add_argument('-f', '--file', help='a filename of english words. \
+        if not given, reads from command line arguments')
+    return parser
+
+if __name__ == '__main__':
+    parser = process_args()
+    args = parser.parse_args()
+    if args.file:
+        EngToKana().fromFile(args.file)
+    elif args.word and len(args.word) > 0:
+        print(args.word)
+        print(EngToKana().fromWordList(args.word))
+    else:
+        parser.print_help(sys.stderr)
---
+++ eng_to_kana_test/test_eng_to_kana.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import unittest
+from eng_to_kana.eng_to_kana import EngToKana
+
+class TestEngToKana(unittest.TestCase):
+    def setUp(self):
+        self.list_func = EngToKana().fromWordList
+        self.file_func = EngToKana().fromFile
+
+    def test_1(self):
+        words = ['what', 'girl', 'cat', 'judge', 'majority']
+        expected = [['ワット', 'ホワット'], ['ガール'], ['キャット'], ['ジャッジ'], ['マジョリティー']]
+        self.assertEqual(expected, self.list_func(words))
+
+    def test_2(self):
+        words = ['gaga']
+        self.assertEqual([[]], self.list_func(words))
